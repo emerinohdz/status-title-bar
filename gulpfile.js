2df778fc295103e5e3478f4aa460dfad0949e27b
Edgar Merino <emerino@nuevebit.com>
2014-05-18 20:13:50 -0500

    added tasks to enable/disable/uninstall extension

--- gulpfile.js
+++ gulpfile.js
@@ -10,19 +10,30 @@ var fs = require('fs');
 var path = require('path');
 var zip = require('gulp-zip');
 var spawn = require('child_process').spawn;
+var exec = require('child_process').exec;
 
 // gulp plugins
 var gulp = require("gulp");
 var clean = require('gulp-clean');
 
+//extension metadata
+var metadata = JSON.parse(fs.readFileSync("src/metadata.json"));
+
 // local config
 var config = {
     srcDir: path.join(__dirname, "src"),
-    distDir: path.join(__dirname, "dist")
+    distDir: path.join(__dirname, "dist"),
+    installDir: path.join(process.env.HOME, ".local/share/gnome-shell/extensions/" + metadata.uuid)
 };
 
-//extension metadata
-var metadata = JSON.parse(fs.readFileSync("src/metadata.json"));
+var enableExtension = function(enable, cb) {
+    var option = enable ? "-e" : "-d";
+    spawn("gnome-shell-extension-tool", [option, metadata.uuid], {stdio: "inherit"})
+            .on("exit", function() {
+                cb();
+            });
+};
+
 
 /**
  * Clean dist dir
@@ -42,19 +53,46 @@ gulp.task("dist", function() {
 });
 
 /**
+ * Copy the extension to local extensions folder only
+ */
+gulp.task("copy:extension", function() {
+    return gulp.src(config.srcDir + "/**/*")
+            .pipe(gulp.dest(config.installDir));
+});
+
+/**
+ * Enable extension.
+ */
+gulp.task("enable", function(cb) {
+    enableExtension(true, cb);
+});
+
+/**
+ * Disable extension.
+ */
+gulp.task("disable", function(cb) {
+    enableExtension(false, cb);
+});
+
+/**
  * Install extension locally.
  */
-gulp.task("install", function() {
-    var dest = path.join(process.env.HOME, ".local/share/gnome-shell/extensions/" + metadata.uuid);
-
-    return gulp.src(config.srcDir + "/**/*")
-            .pipe(gulp.dest(dest));
+gulp.task("install", ["copy:extension"], function(cb) {
+    return gulp.start("enable");
 });
+
+/**
+ * Uninstall extension locally. Removes install dir.
+ */
+gulp.task("uninstall", ["disable"], function(cb) {
+    return gulp.src(config.installDir).pipe(clean({force: true}));
+});
+
 
 /**
  * Restart gnome shell task.
  */
-gulp.task("restart:gnome-shell", ["install"], function() {
+gulp.task("restart:gnome-shell", ["copy:extension"], function() {
     var out = fs.openSync('./out.log', 'a');
     var err = fs.openSync('./out.log', 'a');
     var gs = spawn('gnome-shell', ["-r"], { detached: true });
@@ -72,6 +110,6 @@ gulp.task("restart:gnome-shell", ["install"], function() {
 /**
  * Watch files for changes and reinstall then restart gs
  */
-gulp.task("default", ["install"],  function() {
+gulp.task("default", ["copy:extension"],  function() {
     gulp.watch([config.srcDir + "/**/*"], ["restart:gnome-shell"]);
 });
