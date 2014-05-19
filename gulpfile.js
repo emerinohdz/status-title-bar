58f1ba2fdca13a7b85830ebb40562a034638ce87
Edgar Merino <emerino@nuevebit.com>
2014-05-18 19:52:45 -0500

    added default watch task, when extension source changes extension is reinstalled locally and gnome shell is restarted

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,7 @@
 var fs = require('fs');
 var path = require('path');
 var zip = require('gulp-zip');
+var spawn = require('child_process').spawn;
 
 // gulp plugins
 var gulp = require("gulp");
@@ -35,7 +36,7 @@ gulp.task("clean", function() {
  * Create ZIP file for distribution to gse
  */
 gulp.task("dist", function() {
-    return gulp.src(config.srcDir + "/*")
+    return gulp.src(config.srcDir + "/**/*")
             .pipe(zip(metadata.uuid + ".zip"))
             .pipe(gulp.dest(config.distDir));
 });
@@ -46,6 +47,31 @@ gulp.task("dist", function() {
 gulp.task("install", function() {
     var dest = path.join(process.env.HOME, ".local/share/gnome-shell/extensions/" + metadata.uuid);
 
-    return gulp.src(config.srcDir + "/*")
+    return gulp.src(config.srcDir + "/**/*")
             .pipe(gulp.dest(dest));
+});
+
+/**
+ * Restart gnome shell task.
+ */
+gulp.task("restart:gnome-shell", ["install"], function() {
+    var out = fs.openSync('./out.log', 'a');
+    var err = fs.openSync('./out.log', 'a');
+    var gs = spawn('gnome-shell', ["-r"], { detached: true });
+    gs.stdout.on("data", function(chunk) {
+        process.stdout.write(chunk.toString());
+    });
+    gs.stderr.on("data", function(chunk) {
+        process.stdout.write(chunk.toString());
+    });
+
+    gs.unref();
+
+});
+
+/**
+ * Watch files for changes and reinstall then restart gs
+ */
+gulp.task("default", ["install"],  function() {
+    gulp.watch([config.srcDir + "/**/*"], ["restart:gnome-shell"]);
 });
